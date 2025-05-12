d7df0aca671fa5917fcc4aa740f5dbecc40e252c
denisetiya <denisetiya009@gmail.com>
2025-05-12 08:36:16 +0700

    (feat): Add Card component and product data for display
    - Created a new Card component to showcase product details including name, price, stock, and sold count.
    - Added product data for "Susu Murni 1 Liter" with relevant attributes such as price, stock, category, and image.

---
+++ src/data/product.ts
@@ -0,0 +1,16 @@
+import Susu from '../assets/susu.png';
+
+const productsData = [
+    {
+        name: 'Susu Murni 1 Liter',
+        price: 15000,
+        stock: 50,
+        category: 'Best Seller',
+        sold : 20,
+        image: Susu,
+    }
+]
+
+
+
+export default productsData;
--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,6 +1,45 @@
-function product(){
+import productsData from "../data/product"
+import Card from "../components/widget/card"
+
+export default function product(){
+    
+    
+    
     return(
-        <h1>wdasd</h1>
+        <main className="p-20">
+
+            <h1 className="font-bold text-xl">Produk dengan penjualan tebaik</h1>   
+            <div className="grid grid-cols-4 gap-4 mt-5">
+                {productsData.map((product, index) => (
+                        product.category == "Best Seller" ? (
+                    <Card
+                        key={index}
+                        name={product.name}
+                        price={product.price}
+                        stock={product.stock}
+                        category={product.category}
+                        sold={product.sold}
+                        image={product.image}
+                    />
+                        ) : null
+                ))}
+            </div>
+
+            <h1 className="font-bold text-xl mt-10">List Produk</h1>
+            <div className="grid grid-cols-4 gap-4 mt-5">
+                {productsData.map((product, index) => (
+                    <Card
+                        key={index}
+                        name={product.name}
+                        price={product.price}
+                        stock={product.stock}
+                        category={product.category}
+                        sold={product.sold}
+                        image={product.image}
+                    />
+                ))}
+            </div>
+
+        </main>
     )
 }
-export default product;
