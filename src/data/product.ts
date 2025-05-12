8fe6317b0b13e6acc6aa5e8f46c6d1b66a28e709
MuzakiProject <ahmadhosam2k@gmail.com>
2025-05-12 13:13:23 +0700

    produk

--- src/data/product.ts
+++ src/data/product.ts
@@ -1,4 +1,11 @@
 import Susu from '../assets/susu.png';
+import pepsi from '../assets/[CITYPNG.COM]HD Pepsi Soda Plastic Bottle PNG - 1500x1500 (1).png';
+import cola from '../assets/coca-cola-png-41657.png';
+import redbull from '../assets/pngwing.com (1).png';
+import chitato from '../assets/35e66f3b7abf33cd4c70667bfd359dce (1).png';
+import cheetos from '../assets/pngegg (2).png';
+import lays from '../assets/pngegg (3).png';
+import minyak from '../assets/61631259-314b-4d77-8313-56b0e8c6cc3b.jpg';
 
 const productsData = [
     {
@@ -10,12 +17,60 @@ const productsData = [
         image: Susu,
     },
     {
-        name: 'Susu Murni 1 Liter',
-        price: 15000,
-        stock: 50,
+        name: 'Coca - Cola 1 Liter',
+        price: 8000,
+        stock: 54,
         category: 'Best Seller',
         sold : 20,
-        image: Susu,
+        image: cola,
+    },
+    {
+        name: 'Chitato Dausa Flavor 100gr',
+        price: 12000,
+        stock: 66,
+        category: 'Best Seller',
+        sold : 20,
+        image: chitato,
+    },
+    {
+        name: 'Minyak Goreng 1 Liter',
+        price: 20000,
+        stock: 80,
+        category: 'Best Seller',
+        sold : 20,
+        image: minyak,
+    },
+    {
+        name: 'Pepsi 1 Liter',
+        price: 8000,
+        stock: 48,
+        category: 'Minuman',
+        sold : 20,
+        image: pepsi,
+    },
+    {
+        name: 'Redbull Drink 250ml',
+        price: 10000,
+        stock: 54,
+        category: 'Minuman',
+        sold : 20,
+        image: redbull,
+    },
+    {
+        name: 'Cheetos Party Size 250gr',
+        price: 15000,
+        stock: 24,
+        category: 'Makanan',
+        sold : 40,
+        image: cheetos,
+    },
+    {
+        name: 'Lays Potato 100gr',
+        price: 13000,
+        stock: 54,
+        category: 'Makanan',
+        sold : 20,
+        image: lays,
     }
 ]
 
--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,45 +1,71 @@
 import productsData from "../data/product"
 import Card from "../components/partials/card"
-
+import {
+  Avatar,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar,
+  NavbarBrand,
+} from "flowbite-react";
 export default function product(){
     
     
     
     return(
-        <main className="p-5 md:p-20">
-
-            <h1 className="font-bold text-lg md:text-xl">Best Produk</h1>   
-            <div className="grid grid-cols-2 lg:flex lg:flex-wrap gap-4 mt-5">
-                {productsData.map((product, index) => (
-                        product.category == "Best Seller" ? (
-                    <Card
-                        key={index}
-                        name={product.name}
-                        price={product.price}
-                        stock={product.stock}
-                        category={product.category}
-                        sold={product.sold}
-                        image={product.image}
-                    />
-                        ) : null
-                ))}
-            </div>
-
-            <h1 className="font-bold text-lg md:text-xl mt-10">List Produk</h1>
-            <div className="grid grid-cols-2 lg:flex lg:flex-wrap gap-4 mt-5">
-                {productsData.map((product, index) => (
-                    <Card
-                        key={index}
-                        name={product.name}
-                        price={product.price}
-                        stock={product.stock}
-                        category={product.category}
-                        sold={product.sold}
-                        image={product.image}
-                    />
-                ))}
-            </div>
-
-        </main>
+        <>
+            <Navbar className="border-b border-gray-200 bg-white p-5 sticky top-0 z-40" fluid>
+                <NavbarBrand  href="https://flowbite-react.com">
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Produk</span>
+                </NavbarBrand>
+                <div className="flex md:order-2">
+                    <Dropdown arrowIcon={false} inline label={
+                        <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />}>
+                        <DropdownHeader>
+                            <span className="block text-sm">Bonnie Green</span>
+                            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+                        </DropdownHeader>
+                        <DropdownItem>Dashboard</DropdownItem>
+                        <DropdownItem>Settings</DropdownItem>
+                        <DropdownItem>Earnings</DropdownItem>
+                        <DropdownDivider />
+                        <DropdownItem>Sign out</DropdownItem>
+                    </Dropdown>
+                </div>
+            </Navbar>
+            <main className="p-5 md:p-15">
+                <h1 className="font-bold text-lg md:text-xl">Best Produk</h1>   
+                <div className="grid grid-cols-2 lg:flex lg:flex-wrap gap-4 mt-5 justify-evenly gap-6">
+                    {productsData.map((product, index) => (
+                            product.category == "Best Seller" ? (
+                        <Card
+                            key={index}
+                            name={product.name}
+                            price={product.price}
+                            stock={product.stock}
+                            category={product.category}
+                            sold={product.sold}
+                            image={product.image}
+                        />
+                            ) : null
+                    ))}
+                </div>
+                <h1 className="font-bold text-lg md:text-xl mt-10">List Produk</h1>
+                <div className="grid grid-cols-2 lg:flex lg:flex-wrap gap-4 mt-5 justify-evenly gap-6">
+                    {productsData.map((product, index) => (
+                        <Card
+                            key={index}
+                            name={product.name}
+                            price={product.price}
+                            stock={product.stock}
+                            category={product.category}
+                            sold={product.sold}
+                            image={product.image}
+                        />
+                    ))}
+                </div>
+            </main>
+        </>
     )
 }
